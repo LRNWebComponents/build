4544d43d10eb78386a6b7f354b29b5d6bc8d63d1
btopro <btopro@outlook.com>
2017-05-19 14:32:12 -0400

    updated downstream lrndesign-gallerycard

--- build/default/service-worker.js
+++ build/default/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/index.html","e2ae2b83922083c182f388ec1f43a319"]];
+var precacheConfig = [["/index.html","0222ab1675f43e64ed53371573245058"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
