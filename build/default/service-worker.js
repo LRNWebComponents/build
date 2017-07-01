8008500edc5f4a92867b3972d95d101863e911fa
btopro <btopro@outlook.com>
2017-07-01 01:28:23 -0400

    build updated

--- build/default/service-worker.js
+++ build/default/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","c214008ee52d9828ac8b258060d64ca0"]];
+var precacheConfig = [["index.html","1cb796d7b9301c89425c0537d783bd0c"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
