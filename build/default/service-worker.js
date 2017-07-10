f9e9f85946c055c731720210e57df9e693bfd307
btopro <btopro@outlook.com>
2017-07-09 22:59:16 -0400

    added lrnsys-button

--- build/default/service-worker.js
+++ build/default/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","9252272ff7c6874ded91a8b6484ddc76"]];
+var precacheConfig = [["index.html","0aecc183ac20a8f4856dd3c8fada6ca4"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
