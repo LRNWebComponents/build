0daa380b812b94de948878a19631e358f7e6539e
btopro <btopro@outlook.com>
2017-07-19 17:11:10 -0400

    updated downstream lrnsys-layout

--- build/default/service-worker.js
+++ build/default/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","78bc72b9f390d6bec9e3e17e6edc954c"]];
+var precacheConfig = [["index.html","1eec6de30f687c6269f9f866a1b0a22a"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
