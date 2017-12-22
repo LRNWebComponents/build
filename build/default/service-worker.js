606224b6f8403425be873b5d3eb1c345b9ed4dbc
Bryan btopro Ollendyke <bto108@psu.edu>
2017-12-22 10:32:21 -0500

    updated build dependencies

--- build/default/service-worker.js
+++ build/default/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","afa56e50e867bbeaa7877649c57e39ca"]];
+var precacheConfig = [["index.html","acae775675a2a7d5d5ba8778e57d6faf"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
