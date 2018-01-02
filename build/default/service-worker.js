9e995495b18be4ed1388e212cb1495ac6bb76f7f
Bryan btopro Ollendyke <bto108@psu.edu>
2018-01-02 16:54:40 -0500

    updated downstream elmsln-loading

--- build/default/service-worker.js
+++ build/default/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","e06c1c78f3e4be9a05d0f6ff4d5b6c92"]];
+var precacheConfig = [["index.html","2a464daaeaa039d24702aaa455988a31"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
